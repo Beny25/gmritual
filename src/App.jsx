d0c74f1bf7708d89581bb5bfd2d4e1681b265241
Bandit <88051017+Beny25@users.noreply.github.com>
2025-11-14 04:50:25 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import "./App.css";
+import "./styles/App.css";
 import ConnectWallet from "./components/ConnectWallet";
 import RitualButtons from "./components/RitualButtons";
-import gmbandit from "./assets/gmbandit.jpg";
 
 export default function App() {
   return (
     <div className="wrap">
 
-      <img src={gmbandit} alt="GMbandit Banner"
-        style={{ maxWidth: "100%", borderRadius: 12, marginBottom: 12 }} />
+      <img
+        src="/gmbandit.jpg"
+        alt="GMbandit Banner"
+        style={{ maxWidth: "100%", borderRadius: 12, marginBottom: 12 }}
+      />
 
       <div style={{ color: "#1e3a8a", fontSize: 14, marginBottom: 28 }}>
         This ritual costs only <strong>0.00000033 ETH</strong>.<br />
