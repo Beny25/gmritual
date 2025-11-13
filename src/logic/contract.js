952b0c340e7f1045cef30e7c1e3f3add34608716
Bandit <88051017+Beny25@users.noreply.github.com>
2025-11-14 04:26:09 +0700

    Update contract.js

--- src/logic/contract.js
+++ src/logic/contract.js
@@ -1,18 +1,6 @@
 export const CONTRACT = "0x725Ccb4ddCB715f468b301395Dfd1b1efDb5308A";
 
 export const ABI = [
-  {
-    name: "performRitual",
-    type: "function",
-    stateMutability: "payable",
-    inputs: [{ name: "newMessage", type: "string" }],
-    outputs: []
-  },
-  {
-    name: "fee",
-    type: "function",
-    stateMutability: "view",
-    inputs: [],
-    outputs: [{ type: "uint256" }]
-  }
+  "function performRitual(string calldata newMessage) external payable",
+  "function fee() view returns (uint256)"
 ];
