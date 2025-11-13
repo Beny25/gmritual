9ac3cef04b792568fe6fe65ef75595b6ad94d5fa
Bandit <88051017+Beny25@users.noreply.github.com>
2025-11-13 22:54:19 +0700

    Create constants.js

---
+++ src/logic/constants.js
@@ -0,0 +1,6 @@
+export const CONTRACT = "0x725Ccb4ddCB715f468b301395Dfd1b1efDb5308A";
+
+export const ABI = [
+  "function performRitual(string calldata newMessage) external payable",
+  "function fee() view returns (uint256)"
+];
