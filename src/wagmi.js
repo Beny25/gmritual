1b8763b4cccb181945ddb54115047175178f75d8
Bandit <88051017+Beny25@users.noreply.github.com>
2025-11-14 01:36:32 +0700

    Update wagmi.js

--- src/wagmi.js
+++ src/wagmi.js
@@ -1,14 +1,22 @@
-import { http, createConfig } from "wagmi";
+import { createConfig, http } from "wagmi";
 import { base } from "wagmi/chains";
-import { injected, walletConnect } from "@wagmi/connectors";
+import { injected, walletConnect } from "wagmi/connectors";
 
 export const config = createConfig({
   chains: [base],
-  transports: {
-    [base.id]: http(),
-  },
   connectors: [
     injected(),
-    walletConnect({ projectId: "bandit-temp" }),
+    walletConnect({
+      projectId: import.meta.env.VITE_WC_PROJECT_ID,
+      metadata: {
+        name: "GMRitual",
+        description: "GM Ritual App",
+        url: "https://gmritual.vercel.app",
+        icons: ["https://gmritual.vercel.app/icon.png"],
+      },
+    }),
   ],
+  transports: {
+    [base.id]: http("https://mainnet.base.org"),
+  },
 });
