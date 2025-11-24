0f07591cb681e285071ac4ff17cfef66170a7810
Bandit <88051017+Beny25@users.noreply.github.com>
2025-11-25 00:16:43 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,25 @@ import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { config } from "./wagmi.js";
 
+// ⭐ Coinbase OnchainKit
+import { OnchainKitProvider, walletActions } from "@coinbase/onchainkit";
+import { base } from "wagmi/chains";
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </WagmiProvider>
+    <OnchainKitProvider
+      apiKey={import.meta.env.VITE_COINBASE_API_KEY || ""}
+      chains={[base]}
+      walletConnectVersion="2"
+      walletActions={walletActions}
+    >
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </WagmiProvider>
+    </OnchainKitProvider>
   </React.StrictMode>
 );
